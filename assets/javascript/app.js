d10f0be4d8e296f6abf60f586b5508fd0e182384
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-03-10 14:45:58 -0400

    start button can now go to the first question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,47 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-var questionArr = [
-    {
-        question: "Which fictional city is the home of Batman?",
-        option1: "Atlantis City",
-        option2: "Springfield",
-        option3: "Gotham City",
-        option4: "Zion City"
-    },
-    {
-        question: "Spinach is high in which mineral?",
-        option1: "Zinc",
-        option2: "Iron",
-        option3: "Juice",
-        option4: "Milk"
-    }
-];
+    var questionArr = [
+        {
+            question: "Which fictional city is the home of Batman?",
+            option1: "Atlantis City",
+            option2: "Springfield",
+            option3: "Gotham City",
+            option4: "Zion City"
+        },
+        {
+            question: "Spinach is high in which mineral?",
+            option1: "Zinc",
+            option2: "Iron",
+            option3: "Juice",
+            option4: "Milk"
+        }
+    ];
 
-var index = -1;
+    var index = -1;
 
-function questionFunction(){
-    index ++;
-    $("main").empty();
-    var viewQuestion = $("<div>");
-    viewQuestion.attr("id", "view-question");
-    
-};
+    //  Display the question function after hitting the start button
+    function questionFunction() {
+        index++;
+        $("main").empty();
+        var viewQuestion = $("<div>");
+        viewQuestion.attr("id", "view-question");
+        $("main").append(viewQuestion);
 
-function startpage(){
-    $("main").empty();
-    $("main").append("<button id='start-button'>START!</button>");
-    $("#start-button").on("click",questionFunction);
-};
+        //  Append the question to the div
+        viewQuestion.append("<p id='question-id'>" + questionArr[index].question + "</p>");
+        viewQuestion.append("<p id='option1-id'>" + questionArr[index].option1 + "</p>");
+        viewQuestion.append("<p id='option2-id'>" + questionArr[index].option2 + "</p>");
+        viewQuestion.append("<p id='option3-id'>" + questionArr[index].option3 + "</p>");
+        viewQuestion.append("<p id='option4-id'>" + questionArr[index].option4 + "</p>");
+    };
 
-startpage();
+    //  Display the start button on the start page
+    function startpage() {
+        $("main").empty();
+        $("main").append("<button id='start-button'>START!</button>");
+        $("#start-button").on("click", questionFunction);
+    };
+
+    startpage();
 
 });
